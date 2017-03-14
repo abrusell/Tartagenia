0908e114fc3f41f948704b96715cfa47460a2109
abrusell <a.bruselles@gmail.com>
2017-03-14 15:04:40 +0100

    Update ranking_pipeline.sh
    modificato prioritizer_maxPopAF_ExAC.py in: 
    prioritizer_maxPopAF_ExAC_intervar.py

--- ranking_pipeline.sh
+++ ranking_pipeline.sh
@@ -43,7 +43,7 @@ perl /pico/home/userexternal/aciolfi0/phenolyzer/disease_annotation.pl -f -p --g
 /pico/work/IscrC_FoRWArDS_1/NGS_tools/phenolyzer_score_annotator.py $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP2.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_filtered.vcf" $outdir"Pheno/"$ID".final_gene_list" 2>> $outdir$ID"_ranking_log"
 
 # applico il nostro schema di scoring alle varianti
-/pico/work/IscrC_FoRWArDS_1/NGS_tools/prioritizer_maxPopAF_ExAC.py $phenolist $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP2.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_filtered.phenolyzer.vcf" 2>> $outdir$ID"_ranking_log"
+/pico/work/IscrC_FoRWArDS_1/NGS_tools/prioritizer_maxPopAF_ExAC_intervar.py $phenolist $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP2.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_filtered.phenolyzer.vcf" 2>> $outdir$ID"_ranking_log"
 
 # creo dei file rinominati in maniera piu' sintetica da consegnare ai biologi che hanno windows...
 mkdir $outdir"RENAMED"
