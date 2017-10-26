e8a40d5d92fb316809d67e17300a63c13c1ac456
abrusell <a.bruselles@gmail.com>
2017-10-26 11:58:40 +0200

    small fixes

--- ranking_pipeline_new.sh
+++ ranking_pipeline_new.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 3 ]
 then
-	echo -e "Usage: $0 ID_phenotype_list.txt ID outdir\n\tN.B. In the outdir the file ID_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd_ACMG_DDG2P_Mendel_intervar_db.vcf.gz must be present!"
+	echo -e "\nUsage: $0 ID_phenotype_list.txt ID outdir\n\tN.B. In the outdir the file ID_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd_ACMG_DDG2P_Mendel_intervar_db.vcf.gz must be present!\n"
 	exit 1
 fi
 
@@ -37,7 +37,7 @@ sort -u $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot
 rm $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_filtered.genelist.txt"
 
 # qui uso la lista con i max 4 termini fenotipici che avro' precedentemente creato
-perl /pico/home/userexternal/aciolfi0/phenolyzer/disease_annotation.pl -f -p --gene $ID"_raw_snps-indels_HapCall_genotype_filtered_genelist_sorted.txt" -ph $phenolist -logistic -out "Pheno/"$ID -addon DB_DISGENET_GENE_DISEASE_SCORE,DB_GAD_GENE_DISEASE_SCORE -addon_weight 0.25 2>> $outdir$ID"_ranking_log"
+perl /pico/work/IscrC_FoRWArDS_1/NGS_tools/phenolyzer/disease_annotation.pl -f -p --gene $ID"_raw_snps-indels_HapCall_genotype_filtered_genelist_sorted.txt" -ph $phenolist -logistic -out "Pheno/"$ID -addon DB_DISGENET_GENE_DISEASE_SCORE,DB_GAD_GENE_DISEASE_SCORE -addon_weight 0.25 2>> $outdir$ID"_ranking_log"
 
 # aggiungo il phenolyzer score al vcf
 python /pico/work/IscrC_FoRWArDS_1/NGS_tools/phenolyzer_score_annotator.py $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_filtered.vcf" $outdir"Pheno/"$ID".final_gene_list" 2>> $outdir$ID"_ranking_log"
--- ranking_pipeline_trio_new.sh
+++ ranking_pipeline_trio_new.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 5 ]
 then
-	echo -e "Usage: $0 ID_phenotype_list.txt ID father_ID mother_ID outdir \n       N.B. In the outdir, the files ID_raw_snps-indels_HapCall_genotype_filtered_phased.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd(_spidex)_ACMG_DDG2P_Mendel_intervar_db.vcf.gz and ID_raw_snps-indels_HapCall_genotype_filtered.g.vcf.gz must be present."
+	echo -e "\nUsage: $0 ID_phenotype_list.txt ID father_ID mother_ID outdir\n\tN.B. In the outdir, the files ID_raw_snps-indels_HapCall_genotype_filtered_phased.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd(_spidex)_ACMG_DDG2P_Mendel_intervar_db.vcf.gz and ID_raw_snps-indels_HapCall_genotype_filtered.g.vcf.gz must be present.\n"
     exit 1
 fi
 
@@ -45,7 +45,7 @@ sort -u $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered_phased.g.SnpEff_UC
 rm $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered_phased.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_unphased_filtered.genelist.txt"
 
 # qui uso la lista con i max 4 termini fenotipici che avro' precedentemente creato
-perl /pico/home/userexternal/aciolfi0/phenolyzer/disease_annotation.pl -f -p --gene $ID"_raw_snps-indels_HapCall_genotype_filtered_genelist_sorted.txt" -ph $phenolist -logistic -out "Pheno/"$ID -addon DB_DISGENET_GENE_DISEASE_SCORE,DB_GAD_GENE_DISEASE_SCORE -addon_weight 0.25 2>> $outdir$ID"_ranking_log"
+perl /pico/work/IscrC_FoRWArDS_1/NGS_tools/phenolyzer/disease_annotation.pl -f -p --gene $ID"_raw_snps-indels_HapCall_genotype_filtered_genelist_sorted.txt" -ph $phenolist -logistic -out "Pheno/"$ID -addon DB_DISGENET_GENE_DISEASE_SCORE,DB_GAD_GENE_DISEASE_SCORE -addon_weight 0.25 2>> $outdir$ID"_ranking_log"
 
 # aggiungo il phenolyzer score al vcf
 python /pico/work/IscrC_FoRWArDS_1/NGS_tools/phenolyzer_score_annotator.py $outdir$ID"_raw_snps-indels_HapCall_genotype_filtered_phased.g.SnpEff_UCSCAnnot_dbNSFP3.dbNSFP3_gene_cadd"$spid"_ACMG_DDG2P_Mendel_intervar_db_unphased_filtered.vcf" $outdir"Pheno/"$ID".final_gene_list" 2>> $outdir$ID"_ranking_log"
