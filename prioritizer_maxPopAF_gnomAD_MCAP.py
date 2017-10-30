3a4be0c78203184fd442fbdae09ce85f41356189
aciolfi <aciolfi@gmail.com>
2017-10-30 10:40:23 +0100

    new warnigns
    Added warnings on variants in segmental duplications

--- prioritizer_maxPopAF_gnomAD_MCAP.py
+++ prioritizer_maxPopAF_gnomAD_MCAP.py
@@ -113,6 +113,10 @@ for vcfline in vcf.readlines():
 		else:
 			found_ann = "."
 
+		dups=re.search('segmentDups_UCSC', found_ann)
+		if dups:
+			warn = 1
+
 #		dbsnp=re.search('dbSNP1..\.GMAF=(.+?);', vcfline.rstrip())
 #		if dbsnp:
 #			found_dbsnp = dbsnp.group(1)
@@ -454,13 +458,13 @@ for vcfline in vcf.readlines():
 		if not "." in all_gnomAD_exomes:
 			out.write(vcfline.rstrip()+"\t"+found_var_nt_pos+"\t"+found_var_aa_pos+"\t"+found_maineffect+"\t"+found_genename+"\t"+found_disease_desc+"\t"+found_gene_full_name+"\t"+str(found_cadd)+"\t"+str(found_mcap_pred)+"\t="+found_db+"\t"+str(found_phlyz)+"\t"+str(Final_score)+"\t"+found_intervar+"\t"+found_impact+"\t"+found_feature_id+"\t"+found_ann+"\t"+found_go_biol_proc+"\t"+str(max_gnomAD_exomes_AF)+"\t"+str(found_rvis)+"\t"+str(found_gdi))
 			if warn == 1:
-				out.write("\tMultiple gnomAD_exomes/CADD/SVM values\n")
+				out.write("\tmultiple alleles or low-confidence region\n")
 			else:
 				out.write("\t.\n")
 		else:
 			out.write(vcfline.rstrip()+"\t"+found_var_nt_pos+"\t"+found_var_aa_pos+"\t"+found_maineffect+"\t"+found_genename+"\t"+found_disease_desc+"\t"+found_gene_full_name+"\t"+str(found_cadd)+"\t"+str(found_mcap_pred)+"\t="+found_db+"\t"+str(found_phlyz)+"\t"+str(Final_score)+"\t"+found_intervar+"\t"+found_impact+"\t"+found_feature_id+"\t"+found_ann+"\t"+found_go_biol_proc+"\t"+str(all_gnomAD_exomes)+"\t"+str(found_rvis)+"\t"+str(found_gdi))
 			if warn == 1:
-                                out.write("\tMultiple gnomAD_exomes/CADD/SVM values\n")
+                                out.write("\tmultiple alleles or low-confidence region\n")
                         else:
                                 out.write("\t.\n")
 
